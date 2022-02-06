756ec5157ba84f80cd0c02d1ebabc1c340cb62d2
alessandropec <pec.alessandro@gmail.com>
2022-02-06 20:37:49 +0100

    exttract label fix

--- preprocess/extract_labels.py
+++ preprocess/extract_labels.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     '''
     if (len(sys.argv)==4):
         PATH_REP_PROC,PATH_SUM_PROC,PATH_LABELLED=sys.argv[1],sys.argv[2],sys.argv[3]
-        os.mkdir(PATH_LABELLED)
         label(PATH_REP_PROC,PATH_SUM_PROC,PATH_LABELLED)
 
    
